5b2f59c1d331e65e0356d18b0eca8c22a9373699
Florian Depraz <florian.depraz@arm.com>
2019-05-29 18:53:06 +0100

    ssh: add test for derivation process
    Change-Id: I1d7ff774cb7844b958132736920ef76c615b9841
    Signed-off-by: Florian Depraz <florian.depraz@arm.com>

--- tests/test_ssh.c
+++ tests/test_ssh.c
@@ -246,10 +246,128 @@ static void test_case_ssh_failure_get_data(void)
     DSEC_TEST_ASSERT(dsec_ca_instance_close(&instance) == DSEC_SUCCESS);
 }
 
+static void test_case_full_process(void)
+{
+    TEEC_Session session;
+    TEEC_Context context;
+
+    int32_t result = 0;
+
+    int32_t hh_h_p1 = -1;
+    int32_t hh_h_p2 = -1;
+
+    int32_t ss_h_p1 = -1;
+    int32_t ss_h_p2 = -1;
+
+    uint8_t dh_p1[256];
+    uint32_t dh_p1_size = DSEC_ARRAY_SIZE(dh_p1);
+    uint8_t dh_p2[256];
+    uint32_t dh_p2_size = DSEC_ARRAY_SIZE(dh_p2);
+
+    uint8_t c_p1[256];
+    uint32_t c_p1_size = DSEC_ARRAY_SIZE(c_p1);
+    uint8_t c_p2[256];
+    uint32_t c_p2_size = DSEC_ARRAY_SIZE(c_p2);
+
+    /* Extracted data from Participant 1 */
+    uint8_t sk_p1[256];
+    uint32_t sk_p1_size = DSEC_ARRAY_SIZE(sk_p1);
+    uint8_t c1_p1[256];
+    uint32_t c1_p1_size = DSEC_ARRAY_SIZE(c1_p1);
+    uint8_t c2_p1[256];
+    uint32_t c2_p1_size = DSEC_ARRAY_SIZE(c2_p1);
+
+    /* Extracted data from Participant 2 */
+    uint8_t sk_p2[256];
+    uint32_t sk_p2_size = DSEC_ARRAY_SIZE(sk_p2);
+    uint8_t c1_p2[256];
+    uint32_t c1_p2_size = DSEC_ARRAY_SIZE(c1_p2);
+    uint8_t c2_p2[256];
+    uint32_t c2_p2_size = DSEC_ARRAY_SIZE(c2_p2);
+
+    struct dsec_instance instance = dsec_ca_instance_create(&session, &context);
+    DSEC_TEST_ASSERT(dsec_ca_instance_open(&instance) == DSEC_SUCCESS);
+
+    DSEC_TEST_ASSERT(dsec_hh_create(&hh_h_p1, &instance)  == DSEC_SUCCESS);
+    DSEC_TEST_ASSERT(dsec_hh_create(&hh_h_p2, &instance)  == DSEC_SUCCESS);
+
+    /* Generate the challenge for p1 */
+    result = dsec_hh_challenge_generate(&instance, hh_h_p1, c_p1_size, 1);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    /* Get the challenge for p1 */
+    result = dsec_hh_challenge_get(c_p1, &c_p1_size, &instance, hh_h_p1, 1);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    /* Set p1 challenge to p2 */
+    result = dsec_hh_challenge_set(&instance, hh_h_p2, c_p1, c_p1_size, 2);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+
+    /* Generate the challenge for p2 */
+    result = dsec_hh_challenge_generate(&instance, hh_h_p2, c_p2_size, 1);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    /* Get the challenge for p2 */
+    result = dsec_hh_challenge_get(c_p2, &c_p2_size, &instance, hh_h_p2, 1);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    /* Set p2 challenge to p1 */
+    result = dsec_hh_challenge_set(&instance, hh_h_p1, c_p2, c_p2_size, 2);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+
+    /* Generate DH key pair for p2 */
+    DSEC_TEST_ASSERT(dsec_hh_dh_generate(&instance, hh_h_p2) == DSEC_SUCCESS);
+    /* Get public key of p2 */
+    result = dsec_hh_dh_get_public(dh_p2, &dh_p2_size, &instance, hh_h_p2);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    /* Set p2 public key to p1 */
+    result = dsec_hh_dh_set_public(&instance, hh_h_p1, dh_p2, dh_p2_size);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    /* Generate DH key pair for p1 */
+    DSEC_TEST_ASSERT(dsec_hh_dh_generate(&instance, hh_h_p1) == DSEC_SUCCESS);
+    /* Get public key of p1 */
+    result = dsec_hh_dh_get_public(dh_p1, &dh_p1_size, &instance, hh_h_p1);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    /* Set p1 public key to p2 */
+    result = dsec_hh_dh_set_public(&instance, hh_h_p2, dh_p1, dh_p1_size);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+
+    /* Derive the secrets */
+    result = dsec_ssh_derive(&ss_h_p1, &instance, hh_h_p1);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    DSEC_TEST_ASSERT(ss_h_p1 == 0);
+    result = dsec_ssh_derive(&ss_h_p2, &instance, hh_h_p2);
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    DSEC_TEST_ASSERT(ss_h_p2 == 1);
+
+    /* Get the data and compare the extracted arrays */
+    result = dsec_ssh_get_data(sk_p1, &sk_p1_size,
+                               c1_p1, &c1_p1_size,
+                               c2_p1, &c2_p1_size,
+                               &instance, ss_h_p1);
+
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+
+    result = dsec_ssh_get_data(sk_p2, &sk_p2_size,
+                               c1_p2, &c1_p2_size,
+                               c2_p2, &c2_p2_size,
+                               &instance, ss_h_p2);
+
+    DSEC_TEST_ASSERT(result == DSEC_SUCCESS);
+    DSEC_TEST_ASSERT(sk_p2_size == sk_p1_size);
+    DSEC_TEST_ASSERT(c1_p2_size == c2_p1_size);
+    DSEC_TEST_ASSERT(c2_p2_size == c1_p1_size);
+
+    DSEC_TEST_ASSERT(memcmp(sk_p1, sk_p2, sk_p1_size) == 0);
+    DSEC_TEST_ASSERT(memcmp(c1_p2, c2_p1, c1_p2_size) == 0);
+    DSEC_TEST_ASSERT(memcmp(c2_p2, c1_p1, c2_p2_size) == 0);
+
+    DSEC_TEST_ASSERT(dsec_hh_delete(&instance, hh_h_p1) == DSEC_SUCCESS);
+    DSEC_TEST_ASSERT(dsec_hh_delete(&instance, hh_h_p2) == DSEC_SUCCESS);
+
+    DSEC_TEST_ASSERT(dsec_ca_instance_close(&instance) == DSEC_SUCCESS);
+}
 
 static const struct dsec_test_case_desc test_case_table[] = {
     DSEC_TEST_CASE(test_case_ssh_derive),
     DSEC_TEST_CASE(test_case_ssh_failure_get_data),
+    DSEC_TEST_CASE(test_case_full_process),
 };
 
 const struct dsec_test_suite_desc test_suite = {
