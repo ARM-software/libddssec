7eb718f48c95c4e9f962ca285d4abda5990a0e29
Florian Depraz <florian.depraz@arm.com>
2019-06-19 14:00:19 +0100

    test: FVP: increase timeout
    Increase timeout for the FVP to make sure future tests pass.
    
    Change-Id: Id306a59b0739fa28eff0bbd32e08e3ddc419d1b6
    Signed-off-by: Florian Depraz <florian.depraz@arm.com>

--- tools/testbench.py
+++ tools/testbench.py
@@ -60,7 +60,7 @@ class TestBenchBase:
             while True:
                 # Wait for an expected output or exit using the timeout if the
                 # expected values are not found.
-                index = self.terminal.expect(list(x) + unexpect, timeout=60)
+                index = self.terminal.expect(list(x) + unexpect, timeout=360)
                 if index >= 0 and index < len(unexpect) + 1:
                     # The return index value is one of the given index of the
                     # specified list while calling the expect function
@@ -228,10 +228,10 @@ class TestBenchFVP(TestBenchBase):
         print('Using port {}'.format(self.telnet_port))
 
     def __enter__(self):
-        # Timeout set to 10 minutes, this would be an unusually long test suite
+        # Timeout set to 20 minutes, this would be an unusually long test suite
         self.terminal = pexpect.spawn(
             'telnet localhost {}'.format(self.telnet_port),
-            timeout=600,
+            timeout=1200,
             logfile=sys.stdout.buffer,
             maxread=self.maxread_buffer_size,
             searchwindowsize=self.searchwindow_size,
